cbc09e40dcfea831d16d38c769bd75685a4c63c2
Ivan Petrovic <petraivan1010@gmail.com>
2022-11-04 23:21:48 +0100

    add title clip path animation

--- components/Introduction/Title.tsx
+++ components/Introduction/Title.tsx
@@ -1,12 +1,30 @@
+import { useEffect } from 'react';
 import styles from '../../styles/Introduction.module.css';
 import { TitleProps } from '../../types';
 
 const Title: React.FC<TitleProps> = ({ main, description }) => {
+  useEffect(() => {
+    const style = document.documentElement.style;
+    const clipPath = document.getElementById('clipPath');
+
+    const editCursor = (e: any) => {
+      const { clientX: x, clientY: y } = e;
+      const { x: clipX, y: clipY } = clipPath!.getBoundingClientRect();
+
+      style.setProperty('--x', x - clipX + 'px');
+      style.setProperty('--y', y - clipY + 'px');
+    };
+
+    window.addEventListener('mousemove', editCursor);
+  }, []);
+
   return (
     <div className={styles.title}>
       <h1>
         {main}
-        {/* <div className={styles['clip-path']}>{title}</div> */}
+        <div className={styles['clip-path']} id='clipPath'>
+          {main}
+        </div>
       </h1>
       <p>{description}</p>
     </div>
